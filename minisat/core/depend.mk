00cdffd89570be566647996b982c79b39e889178
Tony Ling <tl2573@columbia.edu>
2014-12-01 00:20:11 -0500

    added minisat solvers

---
+++ minisat/core/depend.mk
@@ -0,0 +1,27 @@
+/home/tony/Downloads/UnifKSATGenerator/minisat/core/Main.o /home/tony/Downloads/UnifKSATGenerator/minisat/core/Main.or /home/tony/Downloads/UnifKSATGenerator/minisat/core/Main.od /home/tony/Downloads/UnifKSATGenerator/minisat/core/Main.op: /home/tony/Downloads/UnifKSATGenerator/minisat/core/Main.cc \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/utils/System.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/IntTypes.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/utils/ParseUtils.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/utils/Options.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Vec.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/XAlloc.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/core/Dimacs.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/core/SolverTypes.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Alg.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Map.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Alloc.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/core/Solver.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Heap.h
+/home/tony/Downloads/UnifKSATGenerator/minisat/core/Solver.o /home/tony/Downloads/UnifKSATGenerator/minisat/core/Solver.or /home/tony/Downloads/UnifKSATGenerator/minisat/core/Solver.od /home/tony/Downloads/UnifKSATGenerator/minisat/core/Solver.op: /home/tony/Downloads/UnifKSATGenerator/minisat/core/Solver.cc \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Sort.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Vec.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/IntTypes.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/XAlloc.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/core/Solver.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Heap.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Alg.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/utils/Options.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/utils/ParseUtils.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/core/SolverTypes.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Map.h \
+ /home/tony/Downloads/UnifKSATGenerator/minisat/mtl/Alloc.h
